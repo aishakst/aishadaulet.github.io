33e6c828652b22a41e356b4e800896c3f2eaf99c
aishakst <96875606+aishakst@users.noreply.github.com>
2022-04-17 13:33:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: 51.1801, lng: 71.4459 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-}
-
-window.initMap = initMap;
+const marker = new google.maps.Marker({
+  position: uluru,
+  map: map,
+});
